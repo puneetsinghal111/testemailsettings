fc064c0acc8f6cdff1d25f1f3f05811e981f7077
Puneet Singhal <puneet@readybytes.in>
2013-03-08 18:22:37 +0530

    plugin created

---
+++ testemail.php
@@ -0,0 +1,110 @@
+<?php
+
+/**
+* @copyright	Copyright (C) 2009 - 2009 Ready Bytes Software Labs Pvt. Ltd. All rights reserved.
+* @license		GNU/GPL, see LICENSE.php
+* @contact		shyam@joomlaxi.com
+*/
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+
+// check if Payplans installed or not
+jimport('joomla.filesystem.file');
+
+if(!defined('DS')){
+	define('DS', DIRECTORY_SEPARATOR);
+}
+
+/**
+ * Payplans System Plugin
+ *
+ * @package	Test Email
+ * @subpackage	Plugin
+ * @author Puneet Singhal
+ * 
+ */
+class  plgSystemTestemail extends XiPlugin
+{
+	public $_app = null;
+	protected $_tplVars = array();
+
+	function __construct(& $subject, $config = array())
+	{
+		parent::__construct($subject, $config);
+		$this->_app = JFactory::getApplication();
+	}
+	
+	function onAfterRoute()
+	{
+		$option = JRequest::getVar('option');
+		$view = JRequest::getVar('view');
+		$from = JRequest::getVar('from_email');
+		
+		if($option != 'com_config' || $view != 'testemail'){
+			return true;
+		}
+		
+		$sender = 'Test User';
+		$subject = 'Congratulations your emailing is working perfectly';
+		$body = 'Hip-hip hurrrayyyyy';
+		
+		if (JFactory::getMailer()->sendMail($from, $sender, $from, $subject, $body) !== true)
+		{
+			return false;
+		}
+		
+		return true;
+	}
+
+	public function onAfterDispatch()
+	{
+		
+	}
+	/**
+	 * Add a image just before </body> tag
+	 * which will href to cron trigger.
+	 */
+	function onAfterRender()
+	{
+		$option = JRequest::getVar('option');
+		if($option != 'com_config'){
+			return true;
+		}
+		?>
+		<script>
+			
+		</script>
+		<?php 
+		// Only render for HTML output
+		if (JFactory::getDocument()->getType() !== 'html' ) { return; }
+		
+		$root = JURI::root();
+		$html = '<script type="text/javascript">';
+			$html .= '(function($){';
+				$html .= '$(document).ready(function(){';
+					$html .= '$("#toolbar").append("<button class=btn type=submit id=jxi_test_email>Test Email</button>");';
+					$html .= '$("#jxi_test_email").click(function(){';
+						$html .= 'var from_email = $("#jform_mailfrom").val();';
+						$html .= 'var url = "'.$root.'index.php?option=com_config&view=testemail&from_email=" + from_email;';
+						$html .= '$(this).load(url); ';
+						//$html .= 'window.location = "http://localhost/payplans2507/index.php?option";';
+					$html .= '});';
+				$html .= '});';
+			$html .= '})(jQuery);';
+		$html .= '</script>';
+		//$html = '<button class="btn btn-success">Test Email</button>';	
+		$body = JResponse::getBody();
+		$body = str_replace('</body>', $html.'</body>', $body);
+		JResponse::setBody($body);
+		
+		}
+
+	function onAfterInitialise()
+	{
+		
+	}
+    
+}
+
+
