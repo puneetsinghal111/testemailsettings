ebf009bda5d0a52bbc0b4703f8ef5dfe33425807
Puneet Singhal <puneet@readybytes.in>
2013-03-09 11:32:33 +0530

    plugin made compitable with Joomla 2.5 also

--- testemail.php
+++ testemail.php
@@ -37,64 +37,70 @@ class  plgSystemTestemail extends JPlugin
 	function onAfterRoute()
 	{
 		$option = JRequest::getVar('option');
-		$view = JRequest::getVar('view');
-		$from = JRequest::getVar('from_email');
+		$from = JRequest::getVar('from_email', false);
 		
-		if($option != 'com_config' || $view != 'testemail'){
+		if($option != 'com_config' || !$from){
 			return true;
 		}
 		
-		$sender = 'Test User';
+		$date = JDate::getInstance()->toSql();
+		
+		$sender  = 'Test User';
 		$subject = 'Congratulations your emailing is working perfectly';
-		$body = 'Hip-hip hurrrayyyyy';
+		$body 	 = 'Hip-hip hurrrayyyyy. Current Time is: '.$date;
 		
 		if (JFactory::getMailer()->sendMail($from, $sender, $from, $subject, $body) !== true)
 		{
 			return false;
 		}
 		
+		if(JVERSION < 3.0){
+			$url = JURI::root().DS.'administrator'.DS.'index.php?option=com_config';
+			JFactory::getApplication()->redirect($url);
+		}
+		
 		return true;
 	}
 
-	/**
-	 * Add a image just before </body> tag
-	 * which will href to cron trigger.
-	 */
 	function onAfterRender()
 	{
 		$option = JRequest::getVar('option');
 		if($option != 'com_config'){
 			return true;
 		}
-		?>
-		<script>
-			
-		</script>
-		<?php 
 		// Only render for HTML output
 		if (JFactory::getDocument()->getType() !== 'html' ) { return; }
 		
 		$root = JURI::root();
-		$html = '<script type="text/javascript">';
-			$html .= '(function($){';
-				$html .= '$(document).ready(function(){';
-					$html .= '$("#toolbar").append("<button class=btn type=submit id=jxi_test_email>Test Email</button>");';
-					$html .= '$("#jxi_test_email").click(function(){';
-						$html .= 'var from_email = $("#jform_mailfrom").val();';
-						$html .= 'var url = "'.$root.'index.php?option=com_config&view=testemail&from_email=" + from_email;';
-						$html .= '$(this).load(url); ';
-						//$html .= 'window.location = "http://localhost/payplans2507/index.php?option";';
+		
+		if(JVERSION < 3.0){
+			$html = '<script type="text/javascript">';
+				$html .= 'function send_email(){';
+					$html .= 'var from_email = document.getElementById("jform_mailfrom").value;';
+					$html .= 'window.location = "'.$root.'administrator/index.php?option=com_config&from_email="+from_email;';
+				$html .= '}';
+				$html .= 'var toolbar = document.getElementById("toolbar");';
+				$html .= 'toolbar.innerHTML = toolbar.innerHTML + "<a id=\"jxi_test_email\" href=\"#\" onclick=\"javascript:send_email();\" style=\"margin:14% 14% 0 0; font-weight:bold; float:right;\">Test Email</a>";';
+			$html .= '</script>';
+		}
+		elseif (JVERSION == 3.0){
+			$html = '<script type="text/javascript">';
+				$html .= '(function($){';
+					$html .= '$(document).ready(function(){';
+						$html .= '$("#toolbar").append("<button class=btn type=submit id=jxi_test_email>Test Email</button>");';
+						$html .= '$("#jxi_test_email").click(function(){';
+							$html .= 'var from_email = $("#jform_mailfrom").val();';
+							$html .= 'var url = "'.$root.'administrator/index.php?option=com_config&from_email=" + from_email;';
+							$html .= '$(this).load(url); ';
+						$html .= '});';
 					$html .= '});';
-				$html .= '});';
-			$html .= '})(jQuery);';
-		$html .= '</script>';
-		//$html = '<button class="btn btn-success">Test Email</button>';	
+				$html .= '})(jQuery);';
+			$html .= '</script>';	
+		}
 		$body = JResponse::getBody();
 		$body = str_replace('</body>', $html.'</body>', $body);
 		JResponse::setBody($body);
 		
-		}
-    
+		return true;
+	}
 }
-
-
